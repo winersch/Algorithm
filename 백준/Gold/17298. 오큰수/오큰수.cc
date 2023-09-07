d3683ca5d4e11f8e2adbe8fdfb59f0d84990fcef
kimsechang <43376841+winersch@users.noreply.github.com>
2023-09-07 14:28:08 +0900

    [Gold IV] Title: 오큰수, Time: 596 ms, Memory: 12380 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+int n;
+int num[1000001], ans[1000001];
+stack<int> st;
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+
+    for (int i = n-1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= num[i]){
+            st.pop();
+        }
+        if (st.empty()){
+            ans[i] = -1;
+        }else{
+            ans[i] = st.top();
+        }
+        st.push(num[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
