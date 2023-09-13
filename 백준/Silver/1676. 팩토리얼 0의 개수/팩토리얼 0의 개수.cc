bd4d519ca252ebcdd3e217725642002d7755ffb0
kimsechang <43376841+winersch@users.noreply.github.com>
2023-09-13 18:56:31 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+    int n, result;
+    cin >> n;
+    result += n/5;
+    result += n/25;
+    result += n/125;
+    cout << result;
+
+
+    return 0;
+}
