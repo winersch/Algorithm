8cdc62e0216364ecdd404aae27b71730429d7c40
kimsechang <43376841+winersch@users.noreply.github.com>
2023-10-12 12:25:20 +0900

    [Silver I] Title: IOIOI, Time: 16 ms, Memory: 12268 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -15,16 +15,35 @@ int main() {
 
     int n, m, count = 0;
     cin >> n >> m;
-    string s, p;
+    string s, p = "";
     cin >> s;
-    p = "IOI";
-    for (int i = 1; i < n; i++) {
-        p += "OI";
+    vector<int> cnt;
+    for (int i = 2; i < m; i++) {
+        if (s[i] == 'I') {
+            if (s[i - 1] == 'O') {
+                if (s[i - 2] == 'I') {
+                    p += "1";
+                } else {
+                    p += s[i];
+                }
+            } else {
+                p += s[i];
+            }
+        }
     }
-    ssize_t found;
-    while ((found = s.find(p)) != string::npos) {
-        count++;
-        s = s.substr(found+2);
+
+    for (int i = 0; i < p.length(); i++) {
+        int c = 0;
+        while (i < p.length() && p[i] == '1') {
+            i++;
+            c++;
+        }
+        cnt.push_back(c);
+    }
+    for (int i = 0; i < cnt.size(); i++) {
+        if (n <= cnt[i]) {
+            count += cnt[i] - n + 1;
+        }
     }
     cout << count;
     return 0;
