3f633479883544b03e149759329d83d37589349f
kimsechang <43376841+winersch@users.noreply.github.com>
2023-10-30 11:59:43 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <map>
+#include <cmath>
+
+
+using namespace std;
+
+int n;
+int graph[101][101];
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (graph[j][i] && graph[i][k]){
+                    graph[j][k] = 1;
+                }
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
