55576b4b1ad4facd869de4044f7efcc0defd3c04
kimsechang <43376841+winersch@users.noreply.github.com>
2024-02-02 17:09:35 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 164 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <iomanip>
+
+#define inf 0x3f3f3f;
+using namespace std;
+
+int n;
+vector<int> v;
+vector<int> ans;
+
+
+int binarySearch(int a) {
+    int mid;
+    int left = 0, right = ans.size() - 1;
+    while (left < right) {
+        mid = (left + right) / 2;
+        if (ans[mid] < a) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return left;
+}
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    ans.push_back(v[0]);
+
+    for (int i = 1; i < n; i++) {
+        if (ans.back() < v[i]){
+            ans.push_back(v[i]);
+        }else{
+            int pos = binarySearch(v[i]);
+            ans[pos] = v[i];
+        }
+    }
+
+    cout << ans.size() << "\n";
+
+    return 0;
+}
