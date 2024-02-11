876a1886143fa92ae5da440bce5451738c68a519
kimsechang <43376841+winersch@users.noreply.github.com>
2024-02-11 20:58:42 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <stack>
+
+using namespace std;
+
+int n;
+vector<int> v;
+int m = 2100000000;
+pair<int, int> ans;
+
+
+void find() {
+    int right = v.size() - 1;
+    int left = 0;
+
+    while (right > left) {
+        int a = v[left];
+        int b = v[right];
+        int tmp = a+b;
+        if (tmp == 0) {
+            ans = {a, b};
+            return;
+        }
+
+        if (abs(tmp) < m) {
+            m = abs(tmp);
+            ans = {a, b};
+        }
+        if (tmp > 0) {
+            right--;
+        } else {
+            left++;
+        }
+
+    }
+
+}
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    find();
+
+    cout << ans.first << " " << ans.second;
+
+    return 0;
+}
