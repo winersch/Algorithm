6ccd432d9d4faec62d4b4c2b55eb20624bbaa519
kimsechang <43376841+winersch@users.noreply.github.com>
2024-04-23 17:29:42 +0900

    [Silver III] Title: 낚이고 낚아라, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/15821. 낚이고 낚아라/낚이고 낚아라.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <stack>
+
+
+#define inf 0x3f3f3f3f
+#define mod 1234567
+#define ll long long
+#define pii pair<int,int>
+
+using namespace std;
+
+int n, k;
+vector<ll> v;
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        ll mx = 0;
+        for (int j = 0; j < a; j++) {
+            int x, y;
+            cin >> x >> y;
+            mx = max(mx, (ll)(x*x + y*y));
+        }
+        v.push_back(mx);
+    }
+    std::sort(v.begin(), v.end());
+    cout.precision(2);
+
+    cout << fixed << (double)v[k-1];
+
+    return 0;
+}
