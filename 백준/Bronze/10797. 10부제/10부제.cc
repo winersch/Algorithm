3b0b452a79deee25ad4f1311abbe60e89e708c74
kimsechang <43376841+winersch@users.noreply.github.com>
2024-04-27 12:59:47 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <stack>
+
+
+#define inf 0x3f3f3f3f
+#define mod 1234567
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+
+using namespace std;
+
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n, cnt = 0;
+    cin >> n;
+    for (int i = 0; i < 5; i++) {
+        int a;
+        cin >> a;
+        if (a%10 == n){
+            cnt++;
+        }
+    }
+    cout << cnt;
+
+    return 0;
+}
