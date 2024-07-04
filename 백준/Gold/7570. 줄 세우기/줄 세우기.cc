a3df8d75861bcfc08e9a283028710d77df49fe41
kimsechang <43376841+winersch@users.noreply.github.com>
2024-07-04 18:34:42 +0900

    [Gold II] Title: 줄 세우기, Time: 116 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <numeric>
+
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+
+using namespace std;
+
+int n, ans;
+int dp[1000001];
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        int a;
+        cin >> a;
+        dp[a] = dp[a-1]+1;
+        ans = max(ans, dp[a]);
+    }
+    cout << n-ans;
+    return 0;
+}
