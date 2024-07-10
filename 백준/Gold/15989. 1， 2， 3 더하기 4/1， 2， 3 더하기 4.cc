70c8e312c5fbbed5f800c9a32fbaf2d77acf4960
kimsechang <43376841+winersch@users.noreply.github.com>
2024-07-10 09:22:26 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <numeric>
+
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+
+using namespace std;
+
+int dp[10001];
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+
+    for (int i = 4; i <= 10000; i++) {
+        dp[i] = dp[i-3] + (i/2) + 1;
+    }
+    int t;
+    cin >> t;
+    while(t--){
+        int a;
+        cin >> a;
+        cout << dp[a] << "\n";
+    }
+    return 0;
+}
