5cc880914fb682354fbcdb31cad743594ae13649
kimsechang <43376841+winersch@users.noreply.github.com>
2024-08-18 14:26:09 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 4 ms, Memory: 41476 KB, Score: 29 point -BaekjoonHub

---
+++ 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <numeric>
+
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+
+using namespace std;
+
+typedef struct city{
+    int t1, m1, t2, m2;
+};
+
+int n, k;
+int dp[101][100001];
+vector<city> v;
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        v.push_back({a,b,c,d});
+    }
+    dp[0][v[0].t1] = v[0].m1;
+    dp[0][v[0].t2] = v[0].m2;
+
+    for (int i = 1; i < n; i++) {
+        city c = v[i];
+
+        for (int j = k; j >= 0; j--) {
+            if (dp[i-1][j] != 0){
+                dp[i][j+c.t1] = max(dp[i][j+c.t1], dp[i-1][j] + c.m1);
+                dp[i][j+c.t2] = max(dp[i][j+c.t2], dp[i-1][j] + c.m2);
+            }
+        }
+    }
+    int ans = 0;
+    for (int i = k; i >= 0; i--) {
+        ans = max(ans, dp[n-1][i]);
+    }
+
+    cout << ans;
+
+    return 0;
+}
