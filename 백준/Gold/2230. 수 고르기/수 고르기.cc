b885de528e282baa8ce16b2a046c4fd124490a9f
kimsechang <43376841+winersch@users.noreply.github.com>
2024-09-04 07:29:08 +0900

    [Gold V] Title: 수 고르기, Time: 16 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <numeric>
+
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+
+using namespace std;
+
+ll n, m, ans = 2100000000;
+ll arr[100001];
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+
+    int left = 0, right = 0;
+
+    while(left < n && right < n){
+        ll tmp = abs(arr[left] - arr[right]);
+        if (tmp < m){
+            right++;
+        }else{
+            ans = min(ans, tmp);
+            left++;
+        }
+    }
+
+
+    cout << ans;
+    return 0;
+}
