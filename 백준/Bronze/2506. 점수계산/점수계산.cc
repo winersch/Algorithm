bac25da4bb5ff5705730a049c0990815a429f07d
kimsechang <43376841+winersch@users.noreply.github.com>
2024-11-23 04:42:20 +0900

    [Bronze III] Title: 점수계산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+
+using namespace std;
+
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n, ans = 0;
+    cin >> n;
+    int st = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if (a == 1){
+            st++;
+            ans += st;
+        }else{
+            st = 0;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
