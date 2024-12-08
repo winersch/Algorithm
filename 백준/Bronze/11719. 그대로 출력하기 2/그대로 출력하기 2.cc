24cc3def5b8f7954a56497d0bc44937397edbaad
kimsechang <43376841+winersch@users.noreply.github.com>
2024-12-09 00:55:54 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+
+using namespace std;
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    string s;
+    while (getline(cin, s))
+    {
+        cout << s << endl;
+    }
+
+
+    return 0;
+}
