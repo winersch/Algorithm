9c7c658a5aa519c102ea79dfd6a82568ea2d7ba0
kimsechang <43376841+winersch@users.noreply.github.com>
2025-04-20 04:27:38 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+
+using namespace std;
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n, k;
+    cin >> n >> k;
+    vi v;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    std::sort(v.begin(), v.end(), greater<>());
+    cout << v[k-1];
+
+
+    return 0;
+}
+
