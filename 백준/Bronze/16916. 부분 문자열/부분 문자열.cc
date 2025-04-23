8be8cd15e5970e42fd98bc2539f24ca00fc50514
kimsechang <43376841+winersch@users.noreply.github.com>
2025-04-23 17:57:45 +0900

    [Bronze II] Title: 부분 문자열, Time: 20 ms, Memory: 8060 KB -BaekjoonHub

---
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <sstream>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+
+using namespace std;
+
+vi getPi(const string &pattern){
+    int m = pattern.size();
+    vi pi(m,0);
+    int j = 0;
+    for (int i = 1; i < m; i++) {
+        while (j > 0 && pattern[i] != pattern[j]){
+            j = pi[j-1];
+        }
+        if (pattern[i] == pattern[j]){
+            pi[i] = ++j;
+        }
+    }
+    return pi;
+}
+
+bool kmp(const string &s, const string &pattern){
+    vi pi = getPi(pattern);
+    int n = s.size(), m = pattern.size();
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        while (j > 0 && s[i] != pattern[j]){
+            j = pi[j-1];
+        }
+        if (s[i] == pattern[j]){
+            if (j == m-1){
+                return true;
+            }else{
+                j++;
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    string a, b;
+    cin >> a >> b;
+
+    cout << (kmp(a,b));
+
+
+    return 0;
+}
+
