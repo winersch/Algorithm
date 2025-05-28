f5c5ded1fa34194cf108c562c8cc3d7a4f17e36e
kimsechang <43376841+winersch@users.noreply.github.com>
2025-05-29 01:53:47 +0900

    [Bronze III] Title: 수도요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10707. 수도요금/수도요금.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <sstream>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+
+using namespace std;
+
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+
+    int a, b, c, d, p;
+    cin >> a >> b >> c >> d >> p;
+    int ans = a * p;
+    if (p <= c){
+        ans = min(ans, b);
+    }else{
+        ans = min(ans, b + (p - c) * d);
+    }
+    cout << ans;
+
+
+    return 0;
+}
