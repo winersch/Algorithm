a7f28310fd4f749473f44a2bd53de21484bee8c6
kimsechang <43376841+winersch@users.noreply.github.com>
2025-06-06 15:40:48 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 696 ms, Memory: 212172 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <sstream>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+
+using namespace std;
+
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    map<string, bool> m;
+    int ans = 0;
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) {
+        for (int j = 1; j + i <= s.length(); j++) {
+            string t = s.substr(i,j);
+            if (!m[t]){
+                m[t] = true;
+                ans++;
+            }
+        }
+    }
+
+    cout << ans;
+
+
+    return 0;
+}
