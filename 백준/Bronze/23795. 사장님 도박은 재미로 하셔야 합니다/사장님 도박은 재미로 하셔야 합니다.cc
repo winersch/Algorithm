7d801bc326a134ad1334db3b5f33bd3ab453ae21
kimsechang <43376841+winersch@users.noreply.github.com>
2025-07-05 12:24:01 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vs vector<string>
+
+using namespace std;
+
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int sum = 0;
+    while(true){
+        int a;
+        cin >> a;
+        if (a == -1){
+            cout << sum;
+            break;
+        }
+        sum += a;
+    }
+
+
+    return 0;
+}
