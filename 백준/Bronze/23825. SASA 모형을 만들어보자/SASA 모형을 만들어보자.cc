8708f47434e505b4d2ed8457559fcd8610605a35
kimsechang <43376841+winersch@users.noreply.github.com>
2025-07-12 02:59:58 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vs vector<string>
+
+using namespace std;
+
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n, m;
+    cin >> n >> m;
+    cout << min(n,m)/2;
+    return 0;
+}
