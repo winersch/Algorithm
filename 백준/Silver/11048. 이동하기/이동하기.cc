eb3fe926812af9a5e5aa5b90658c935d635a5877
kimsechang <43376841+winersch@users.noreply.github.com>
2025-09-07 05:20:57 +0900

    [Silver II] Title: 이동하기, Time: 68 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+#include <regex>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vs vector<string>
+
+using namespace std;
+
+int n, m;
+int grid[1001][1001];
+int dp[1001][1001];
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    dp[0][0] = grid[0][0];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i > 0){
+                dp[i][j] = max(dp[i][j], dp[i-1][j] + grid[i][j]);
+            }
+            if (j > 0){
+                dp[i][j] = max(dp[i][j], dp[i][j-1] + grid[i][j]);
+            }
+        }
+    }
+
+    cout << dp[n-1][m-1];
+
+    return 0;
+}
