3832753da1420adc815424981627c0c24f92d54c
kimsechang <43376841+winersch@users.noreply.github.com>
2025-09-23 21:43:19 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+#include <regex>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vs vector<string>
+
+using namespace std;
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    ll n; cin >> n;
+    if (n%2 == 0){
+        cout << "CY";
+    }else{
+        cout << "SK";
+    }
+
+    return 0;
+}
