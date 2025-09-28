1373f6269300ec562c91074bf77b8f87ca11469c
kimsechang <43376841+winersch@users.noreply.github.com>
2025-09-28 23:38:54 +0900

    [Silver I] Title: 단어 맞추기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9081. 단어 맞추기/단어 맞추기.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+#include <regex>
+
+#define inf 0x3f3f3f3f
+#define ll long long
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vs vector<string>
+
+using namespace std;
+
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int t; cin >> t;
+    while(t--){
+        string st; cin >> st;
+        string s = st;
+        if(std::next_permutation(s.begin(), s.end())){
+            cout << s << "\n";
+        }else{
+            cout << st << "\n";
+        }
+    }
+
+    return 0;
+}
